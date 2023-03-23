3655cc9c199ff9066f1ef9dd646494f7e2e7c855
Julia <kuuerusa@gmail.com>
2023-03-23 22:17:35 +0100

    admin_anuncios.blade added meta

--- resources/views/admin/admin_anuncios.blade.php
+++ resources/views/admin/admin_anuncios.blade.php
@@ -6,6 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'MiLorito') }}</title>
